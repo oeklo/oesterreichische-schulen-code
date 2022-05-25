e6632eeb3be151a08647652af05759b74b0c9774
Raphael Krupinski <10319569-mattesilver@users.noreply.gitlab.com>
2022-05-25 20:00:20 +0200

    🐛 Fix errors on changing between bundeslands

--- austrian_schools/spiders/schools.py
+++ austrian_schools/spiders/schools.py
@@ -1,6 +1,14 @@
 import re
+from typing import Tuple, Optional, NamedTuple
 
 import scrapy
+import scrapy.signals
+
+
+class SearchSummary(NamedTuple):
+    from_: int
+    to: int
+    sum: int
 
 
 class SchoolsSpider(scrapy.Spider):
@@ -8,35 +16,87 @@ class SchoolsSpider(scrapy.Spider):
     allowed_domains = ['www.schulen-online.at']
     start_urls = ['https://www.schulen-online.at/sol/oeff_suche_schulen.jsf']
 
-    def __init__(self, **kwargs):
+    def __init__(self, continue_from: str, **kwargs):
         super().__init__(**kwargs)
         self.lands = None
+        self.results_per_page = 50
+        if continue_from:
+            self.continue_from = process_output_file(continue_from)
 
-    def parse(self, response: scrapy.http.HtmlResponse, land_idx=0, **kwargs):
+    def parse(self, response: scrapy.http.HtmlResponse, land_idx=0, continue_from_code: Optional[str] = None):
+        self.logger.info('parse (search form): land: %i; continue_from: %s' % (land_idx, continue_from_code))
         if not self.lands:
+            self.logger.debug('Load lands')
             self.lands = response.css('#myform1\\:bundesland option').xpath('text()').getall()[1:]
-            print(self.lands)
 
-        request = scrapy.FormRequest.from_response(
+        if self.continue_from:
+            land_idx = self.lands.index(self.continue_from[0])
+            continue_from_code = self.continue_from[1]
+        else:
+            continue_from_code = None
+        self.continue_from = None
+
+        yield scrapy.FormRequest.from_response(
             response,
             'myform1',
             formdata={
-                'myform1:anz': '50',
+                'myform1:anz': str(self.results_per_page),
                 'myform1:bundesland': str(land_idx + 1),
                 # there's a bug somewhere, scrapy sets here "on"
                 'myform1:art': "",
             },
-            callback=self.parse_search_results,
+            callback=self.handle_search_results,
+            cb_kwargs={
+                'land_idx': land_idx,
+                'continue_from_code': continue_from_code
+            },
         )
-        yield request
+
+    def handle_search_results(
+            self, response: scrapy.http.HtmlResponse, idx=0, land_idx=0, continue_from_code: Optional[str] = None
+    ):
+        self.logger.info('land: %i; idx: %i; continue_from: %s' % (land_idx, idx, continue_from_code))
+        tbl = response.xpath('//div[@id="tabs-2"]//table[@class="ergebnisTable"]/tbody')
+        results_on_page = int(float(tbl.xpath('count(tr)').get()))
+
+        if continue_from_code:
+            continue_from_idx = tbl.xpath('count(tr[.//a/text()=$code]/preceding-sibling::tr)',
+                                          code=continue_from_code).get()
+
+            if continue_from_idx is not None:
+                continue_from_idx = int(float(continue_from_idx))
+                self.logger.info('Found item: %i' % continue_from_idx)
+                # continue CRAWLING with the next row on this page
+                yield self.continue_search_results(
+                    response,
+                    continue_from_idx,
+                    land_idx,
+                    last_item=continue_from_idx >= results_on_page - 1
+                )
+            else:
+                # continue SEARCHING for the first page to crawl
+                yield self.continue_search_results(
+                    response,
+                    results_on_page - 1,
+                    land_idx,
+                    continue_from_code,
+                    last_item=True
+                )
+
+            return
+
+        yield self.parse_search_results(response, idx, land_idx)
 
     def parse_search_results(self, response: scrapy.http.HtmlResponse, idx=0, land_idx=0):
-        # 1-based
-        onclick = response.xpath('//div[@id="tabs-2"]//table[@class="ergebnisTable"]/tbody/tr[$index]/td//a/@onclick',
-                                 index=idx + 1).get()
+        parent = response.xpath('//div[@id="tabs-2"]//table[@class="ergebnisTable"]/tbody')
+        results_on_page = int(float(parent.xpath('count(tr)').get()))
 
-        form_id, idcl = re.match(r"return myfaces.oam.submitForm\('([^']+)','([^']+)'\);", onclick).groups()
-        yield scrapy.FormRequest.from_response(
+        # 1-based
+        onclick = parent.xpath('tr[$index]/td//a/@onclick', index=idx + 1).get()
+        self.logger.debug(onclick)
+
+        form_id, idcl = re.match(r"^return myfaces.oam.submitForm\('([^']+)','([^']+)'\);$", onclick).groups()
+        return scrapy.FormRequest.from_response(
             response,
             form_id,
             formdata={
@@ -46,10 +106,21 @@ class SchoolsSpider(scrapy.Spider):
             cb_kwargs={
                 'idx': idx,
                 'land_idx': land_idx,
+                'last_item': idx == results_on_page - 1,
             },
         )
 
-    def parse_school(self, response: scrapy.http.HtmlResponse, idx: int, land_idx):
+    def handle_school_details(self, response: scrapy.http.HtmlResponse, idx: int, land_idx: int,
+                              last_item: bool = False):
+        yield self.parse_school(response, idx, land_idx)
+
+        next_ = self.continue_search_results(response, idx, land_idx, last_item=last_item)
+        if next_:
+            yield next_
+
+    def parse_school(self, response: scrapy.http.HtmlResponse, idx: int, land_idx: int):
+        search_summary = self.get_search_summary(response)
+        self.logger.info('land: %i; school: %i / %i' % (land_idx, search_summary.from_ + idx, search_summary.sum))
         item = {
             'Bundesland': self.lands[land_idx]
         }
@@ -66,26 +137,55 @@ class SchoolsSpider(scrapy.Spider):
             item[key] = value
         yield item
 
-        if idx < 49:
-            for request in self.parse_search_results(response, idx + 1):
-                yield request
-        else:
+    def continue_search_results(self, response, idx: int, land_idx: int, continue_from_code: Optional[str] = None,
+                                last_item: bool = False) -> Optional[scrapy.Request]:
+        """
+        The only occasion when continue_from_code is passed here, is when searching for the first page after the last
+        item in the input file, meaning, we only use it for the next page.
+
+        :param last_item - the last processed item was last on the page
+        """
+
+        if last_item:
             if self.is_last_page(response):
-                for request in self.parse(response, land_idx + 1):
-                    yield request
+                # next bundesland
+                if land_idx + 1 < len(self.lands):
+                    return self.parse(response, land_idx + 1).__next__()
+
             else:
-                yield scrapy.FormRequest.from_response(
+                # next page
+                return scrapy.FormRequest.from_response(
                     response,
                     'j_id_20',
                     formdata={
                         'j_id_20:_idcl': "j_id_20:next"
                     },
                     callback=self.parse_search_results,
+                    cb_kwargs={
+                        'land_idx': land_idx,
+                        'continue_from_code': continue_from_code,
+                    },
                 )
+        else:
+            # next school
+            return self.parse_search_results(response, idx + 1, land_idx)
 
-    def is_last_page(self, response:scrapy.http.HtmlResponse)->bool:
+    def is_last_page(self, response: scrapy.http.HtmlResponse) -> bool:
+        summary = self.get_search_summary(response)
+        return summary.to == summary.sum
+
+    @staticmethod
+    def get_search_summary(response: scrapy.http.HtmlResponse) -> SearchSummary:
         parent = response.css('#j_id_20 .buttonframe')
-        last_page_idx=int(parent.xpath('.//*[@id="j_id_20:to"]/text()').get())
-        last_idx=int(parent.xpath('.//*[@id="j_id_20:sum"]/text()').get())
-        self.log('Page %i of %i'%(last_page_idx, last_idx))
-        return last_page_idx == last_idx
+        return SearchSummary(
+            int(parent.xpath('.//*[@id="j_id_20:from"]/text()').get()),
+            int(parent.xpath('.//*[@id="j_id_20:to"]/text()').get()),
+            int(parent.xpath('.//*[@id="j_id_20:sum"]/text()').get()),
+        )
+
+
+def process_output_file(path: str) -> Tuple[str, str]:
+    with open(path, 'r') as fp:
+        for count, line in enumerate(fp):
+            pass
+    return tuple(line.split(',')[:2])
